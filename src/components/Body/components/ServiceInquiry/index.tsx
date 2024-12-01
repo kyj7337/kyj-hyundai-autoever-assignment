98e44cbbcb6f2ac1c48a16d8bc1e0fa3d0506451
youngjun kim <youngjunkim@youngjunui-MacBookPro.local>
2024-12-01 20:32:17 +0900

    상품제안서링크 다운으로 수정

--- src/components/Body/components/ServiceInquiry/index.tsx
+++ src/components/Body/components/ServiceInquiry/index.tsx
@@ -8,31 +8,27 @@ interface InquiryItem {
   icon: ReactElement;
   name: string;
   desc?: string;
-  onClick?: () => void;
+  link: string;
+  download?: boolean;
 }
 
 const items: InquiryItem[] = [
   {
     icon: <img src={documentIcon} className='inquiry-ic' />,
     name: '상품제안서 다운로드',
-    onClick: () => {
-      window.open('/product_proposal.pdf', '_blank');
-    },
+    link: '/product_proposal.pdf',
+    download: true,
   },
   {
     icon: <img src={pencilIcon} className='inquiry-ic' />,
     name: '상담문의 등록하기',
-    onClick: () => {
-      window.open('https://wiblebiz.kia.com/Counsel', '_blank');
-    },
+    link: 'https://wiblebiz.kia.com/Counsel',
   },
   {
     icon: <img src={kakaoIcon} className='inquiry-ic' />,
     name: '카톡으로 문의하기',
     desc: 'ID: Wible Biz(위블 비즈)',
-    onClick: () => {
-      window.open('https://pf.kakao.com/_xfLxjdb', '_blank');
-    },
+    link: 'https://pf.kakao.com/_xfLxjdb',
   },
 ];
 
@@ -43,13 +39,18 @@ export default function ServiceInquiry() {
       <div className='inquiry-box'>
         {items.map((item) => {
           return (
-            <div className='inquiry-item' key={item.name} onClick={item.onClick}>
+            <a
+              href={item.link}
+              download={item.download && 'wiblebiz 상품제안서.pdf'}
+              className='inquiry-item'
+              key={item.name}
+            >
               {item.icon}
               <div className='desc-txt'>
                 <div className='desc'>{item.name}</div>
                 {item.desc && <div className='sub-desc'>{item.desc}</div>}
               </div>
-            </div>
+            </a>
           );
         })}
       </div>
