d32f69bd9e2d3cef11f9bae2db70a2ffbb828890
youngjun kim <youngjunkim@youngjunui-MacBookPro.local>
2024-12-03 08:59:53 +0900

    custom hook 테스트 코드 추가, README 수정

--- src/test/integration-test/index.tsx
+++
@@ -1,8 +0,0 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactElement } from 'react';
-
-export const hookWrapper = () => {
-  return ({ children }: { children: ReactElement }) => {
-    return <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>;
-  };
-};
--- src/test/integration-test/useResetCategoryId.test.tsx
+++ src/test/integration-test/useResetCategoryId.test.tsx
@@ -1,12 +1,25 @@
 import { describe, expect, it } from 'vitest';
 import { useSearchStore } from '@/store/useSearchStore';
 import { renderHook } from '@testing-library/react';
+import useResetCategoryId from '@/components/Body/components/SearchAndFaq/Faq/hooks/useResetCategoryId';
 
 describe('useResetCategoryId 테스트', () => {
-  it('실행되기 전 다른 값 세팅', () => {
+  it('실행되기 전 다른 값 세팅 후 useResetCategoryId 실행', () => {
     const { result: searchStoreResult } = renderHook(() => useSearchStore());
-    const { categoryId } = searchStoreResult.current;
-    console.log(categoryId);
-    expect(categoryId).toBe(1);
+    const { categoryId, setCategoryId } = searchStoreResult.current;
+    // * 1. 초기값확인
+    expect(categoryId).toBe('');
+    // * 2. 임의의 값 설정
+    setCategoryId('임시값 설정');
+    const { result: searchStoreResult2 } = renderHook(() => useSearchStore());
+    const { categoryId: categoryId2 } = searchStoreResult2.current;
+    // * 3. 임의의값 확인
+    expect(categoryId2).toBe('임시값 설정');
+    // * 4. 커스텀 훅 실행
+    renderHook(() => useResetCategoryId('intro'));
+    const { result: searchStoreResult3 } = renderHook(() => useSearchStore());
+    const { categoryId: categoryId3 } = searchStoreResult3.current;
+    // * 5. 값 초기화 확인
+    expect(categoryId3).toBe('');
   });
 });
