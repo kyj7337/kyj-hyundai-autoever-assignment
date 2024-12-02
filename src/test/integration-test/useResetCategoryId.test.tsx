e5589011ce04195bea43680d2864398569ec51fa
youngjun kim <youngjunkim@youngjunui-MacBookPro.local>
2024-12-03 08:44:37 +0900

    testing library 환경설정

--- src/test/integration-test/useResetCategoryId.test.tsx
+++ src/test/integration-test/useResetCategoryId.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { useSearchStore } from '@/store/useSearchStore';
 import { renderHook } from '@testing-library/react';
 
@@ -6,5 +6,7 @@ describe('useResetCategoryId 테스트', () => {
   it('실행되기 전 다른 값 세팅', () => {
     const { result: searchStoreResult } = renderHook(() => useSearchStore());
     const { categoryId } = searchStoreResult.current;
+    console.log(categoryId);
+    expect(categoryId).toBe(1);
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,21 @@ import vitestConfig from './vitest.config';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.ttf'],
-  server: {
-    port: 3000,
-    open: 'http://localhost:3000',
-  },
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default mergeConfig(
+  vitestConfig,
+  defineConfig({
+    plugins: [react()],
+    assetsInclude: ['**/*.ttf'],
+    server: {
+      port: 3000,
+      open: 'http://localhost:3000',
     },
-  },
-});
-// export default mergeConfig(
-//   vitestConfig,
-//   defineConfig({
-//     plugins: [react()],
-//     assetsInclude: ['**/*.ttf'],
-//     server: {
-//       port: 3000,
-//       open: 'http://localhost:3000',
-//     },
 
-//     resolve: {
-//       alias: {
-//         '@': path.resolve(__dirname, 'src'),
-//       },
-//     },
-//   })
-// );
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  })
+);
